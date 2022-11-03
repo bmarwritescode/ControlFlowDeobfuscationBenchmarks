d1a1569b26edf5ee8f3222df19417d9f38abeeb4
Ziteng Zetten Wang <z@wzt.me>
2022-11-03 20:46:11 +0000

    more

--- tigress-tool/run.py
+++ tigress-tool/run.py
@@ -1,22 +1,26 @@
-import argparse
+from pathlib import Path
+from shutil import copy, which
+from dataclasses import dataclass
+from argparse import ArgumentParser, Namespace
+from itertools import dropwhile
+
 import os
 import subprocess
 import yaml
 
-from pathlib import Path
-from shutil import copy, which
-from collections import namedtuple
-from dataclasses import dataclass
-
-# specify tigress path if not installed
 TIGRESS_HOME = "./tigress/3.3.2"
-TIGRESS_ENV = "--Environment=x86_64:Linux:Gcc:4.6"
+TIGRESS_ENV = "x86_64:Linux:Gcc:4.6"
 TIGRESS_PARAMS = [f"--Environment={TIGRESS_ENV}"]
 
+FAKE_LIBC_INCLUDE = "./fake_libc_include"
+
+
 @dataclass(frozen=True)
+class TigressVariant:
     name: str
-    func: str
     procedure: list[list[str]]
+    args: dict[str, str]
+    format: bool
 
 
 @dataclass(frozen=True)
@@ -26,23 +30,27 @@ class TigressConfig:
     variants: list[TigressVariant]
 
 
-def get_args():
+def get_args() -> Namespace:
     """
     Parse command line arguments
     """
-    parser = argparse.ArgumentParser(
-        description="Iteratively apply Tigress transformations to C files")
+    parser = ArgumentParser(
+        description="Iteratively apply Tigress transformations to C files"
+    )
+    parser.add_argument("--config", "-c", help="Path to config file", required=True)
     parser.add_argument(
-        "--config", "-c", help="Path to config file", required=True)
+        "--output", "-o", help="Path to output directory", required=True
+    )
     parser.add_argument(
-        "--output", "-o", help="Path to output directory", required=True)
-    parser.add_argument("--input-dir", "-i",
-                        help="Path to input directory", required=True)
+        "--input-dir", "-i", help="Path to input directory", required=True
+    )
 
     return parser.parse_args()
 
 
-def call_tigress(tigress_home, params, src_file, out_file):
+def call_tigress(
+    tigress_home: str, params: list[str], src_file: str, out_file: str
+) -> str:
     # setup Tigress environment and path
     tigress_exec = which("tigress")
     if tigress_exec is None:
@@ -50,16 +58,16 @@ def call_tigress(tigress_home, params, src_file, out_file):
         tigress_exec = os.path.join(tigress_home, "tigress")
 
     try:
-        output = subprocess.run([tigress_exec, f"--out={out_file}"] + TIGRESS_PARAMS + params + [src_file],
-                                encoding="utf8",
-                                check=True,
-                                stdout=subprocess.PIPE,
-                                stderr=subprocess.PIPE).stdout
+        output = subprocess.run(
+            [tigress_exec, f"--out={out_file}"] + TIGRESS_PARAMS + params + [src_file],
+            encoding="utf8",
+            check=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+        ).stdout
         return output.strip()
     except subprocess.CalledProcessError as ex:
-        print(ex.output)
-        print(ex.stderr)
-        print(ex)
+        return "".join([ex.output, ex.stderr, str(ex)])
 
 
 def run_tigress(config: TigressConfig, source_dir: Path, output_dir: Path) -> None:
@@ -84,11 +92,64 @@ def run_tigress(config: TigressConfig, source_dir: Path, output_dir: Path) -> No
 
             # sequentially apply transformation to C file
             for proc in variant.procedure:
-                call_tigress(TIGRESS_HOME, config.params +
-                             proc, src_in_file, src_out_file)
+                proc = [p.format(**variant.args) for p in proc]
+
+                out = call_tigress(
+                    TIGRESS_HOME,
+                    config.params + proc,
+                    str(src_in_file),
+                    str(src_out_file),
+                )
+                if out:
+                    print(out)
                 copy(src_out_file, src_in_file)
 
             os.remove(src_in_file)
+
+            if variant.format:
+                format_c_source(str(src_out_file))
+
+
+def format_c_source(src_path: str) -> None:
+    FORMAT_COMMAND = ["clang-format", "--style=Google", "-i", src_path]
+    PREPROCESS_COMMAND = [
+        "gcc",
+        "-E",
+        f"-I{FAKE_LIBC_INCLUDE}",
+        src_path,
+    ]
+    HEADERS = """#define __attribute__(x)
+#define __extension__
+#define __restrict
+#define __inline
+#define __asm__(x)
+
+typedef void* __builtin_va_list;"""
+    LINE_AFTER_HEADERS = "typedef unsigned short ushort;"
+
+    with open(src_path, "r") as src_file:
+        src_lines = src_file.readlines()
+
+    with open(src_path, "w") as src_file:
+        src_file.write(HEADERS)
+        src_file.writelines(src_lines)
+
+    # preprocess the source file for headers, comments removal etc.
+    out_preproc = subprocess.check_output(PREPROCESS_COMMAND)
+    src_lines = out_preproc.decode("utf8").splitlines()
+
+    # remove lines that are not part of the source code
+    src_lines = [
+        line
+        for line in src_lines# dropwhile(lambda x: x != LINE_AFTER_HEADERS, src_lines)
+        if not line.startswith("#") and line.strip()
+    ]
+
+    with open(src_path, "w") as f:
+        f.write("\n".join(src_lines))
+
+    # format the source file
+    subprocess.run(FORMAT_COMMAND, check=True)
 
 
 if __name__ == "__main__":
@@ -96,8 +157,9 @@ if __name__ == "__main__":
     args = get_args()
     with open(args.config, "rb") as f:
         config = yaml.safe_load(f.read()) or {}
-        config["variants"] = [TigressVariant(
-            **variant) for variant in config["variants"]]
+        config["variants"] = [
+            TigressVariant(**variant) for variant in config["variants"]
+        ]
         config = TigressConfig(**config)
 
     source_dir = Path(args.input_dir).resolve()
